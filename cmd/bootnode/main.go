b00d7dc1c7ae84ed53ad941e246d7ee6c9a97ed4
ufgwallet <ufgwallet@gmail.com>
2025-04-16 17:50:03 +0530

    Update main.go

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -1,5 +1,5 @@
 // Copyright 2015 The go-ethereum Authors
-// This file is part of go-ethereum.
+// This file iss part of go-ethereum.
 //
 // go-ethereum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
